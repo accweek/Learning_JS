e2055d4715ba2781587fd5b6cade40fe0e94b6b9
Pavel Loginov <43883147+accweek@users.noreply.github.com>
2022-09-22 12:48:16 +0400

    calculator v2

--- Calc.js
+++ Calc.js
@@ -1,54 +1,31 @@
-var buttonPlus = document.getElementById('buttonPlus');
-var buttonMinus = document.getElementById('buttonMinus');
-var buttonMultiply = document.getElementById('buttonMultiply');
-var buttonDevide = document.getElementById('buttonDevide');
+var operationButtons = document.getElementsByClassName('operationButtons')
 
-//function operation()
+var input1 = document.getElementById('number1');
+var input2 = document.getElementById('number2');
 
-function onButtonPlusClick() {
-    var input1 = document.getElementById('number1');
-    var input2 = document.getElementById('number2');
-    
+function makeOperation(operationCode){
     var number1 = Number(input1.value);
     var number2 = Number(input2.value);
 
-    var summ = number1 + number2;
-    window.alert(summ);
-}
-function onButtonMinusClick() {
-    var input1 = document.getElementById('number1');
-    var input2 = document.getElementById('number2');
-    
-    var number1 = Number(input1.value);
-    var number2 = Number(input2.value);
-
-    var summ = number1 - number2;
-    window.alert(summ);
-}
-function onButtonMultiplyClick() {
-    var input1 = document.getElementById('number1');
-    var input2 = document.getElementById('number2');
-    
-    var number1 = Number(input1.value);
-    var number2 = Number(input2.value);
-
-    var summ = number1 * number2;
-    window.alert(summ);
-}
-function onButtonDevideClick() {
-    var input1 = document.getElementById('number1');
-    var input2 = document.getElementById('number2');
-    
-    var number1 = Number(input1.value);
-    var number2 = Number(input2.value);
-
-    var summ = number1 / number2;
-    window.alert(summ);
+    if (operationCode === '+'){
+        var result = number1 + number2;
+    } else if (operationCode === '-'){
+        result = number1 - number2;
+    } else if (operationCode === '*'){
+        result = number1 * number2;
+    } else {
+        result = number1 / number2;
+    } 
+    window.alert(result);
 }
 
-buttonPlus.addEventListener('click', onButtonPlusClick);
-buttonMinus.addEventListener('click', onButtonMinusClick);
-buttonMultiply.addEventListener('click', onButtonMultiplyClick);
-buttonDevide.addEventListener('click', onButtonDevideClick);
+function onOperationButtonClick(eventObject){
+    var clickedElement = eventObject.currentTarget;
+    var operation = clickedElement.innerHTML;
+    makeOperation(operation);
+}
 
-// IntelliCode
+for (var i = 0; i < operationButtons.length; i++) {
+    var button = operationButtons[i];
+    button.addEventListener('click',onOperationButtonClick);    
+}
