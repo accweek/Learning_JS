2ddbeb1a87d1e729a0b5ade3b9cf3ba31f5a02fa
Pavel Loginov <43883147+accweek@users.noreply.github.com>
2022-09-27 14:27:43 +0400

    new version - slider (constructor + new)
    new version sliders implemented through the constructor

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var slider1 = sliderFactory.createNewSlider();
-var slider2 = sliderFactory.createNewSlider();
+var slider1 = new Slider();
+var slider2 = new Slider();
 
 slider1.start('slider1');
 slider2.start('slider2');
